110637d1570770ee91ec4afc01d99a3e40bcd890
PatrickARatcliff <pratcliff5@gmail.com>
2022-11-15 22:31:27 -0700

    debug auth issue, bcrypt on user.bulk create

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -18,20 +18,21 @@ router.post('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
   try {
+    console.log('hey look!')
     const userData = await User.findOne({ where: { email: req.body.email } });
 
     if (!userData) {
       res
-        .status(400)
+        .status(401)
         .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
 
     const validPassword = await userData.checkPassword(req.body.password);
-
+    console.log(req.body.password)
     if (!validPassword) {
       res
-        .status(400)
+        .status(600)
         .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
@@ -44,7 +45,7 @@ router.post('/login', async (req, res) => {
     });
 
   } catch (err) {
-    res.status(400).json(err);
+    res.status(420).json(err);
   }
 });
 
--- models/Comment.js
+++ models/Comment.js
@@ -22,13 +22,13 @@ Comment.init(
         key: 'id',
       },
     },
-    post_id: {
-      type: DataTypes.STRING,
-      references: {
-        model: 'post',
-        key: 'id',
-      },
-    },
+    // post_id: {
+    //   type: DataTypes.STRING,
+    //   references: {
+    //     model: 'post',
+    //     key: 'id',
+    //   },
+    // },
   },
   {
     sequelize,
