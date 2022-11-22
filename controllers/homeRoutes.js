d090072d2f2000c7524ffc157266ef6ac20a756c
PatrickARatcliff <pratcliff5@gmail.com>
2022-11-21 23:19:37 -0700

    attempt to debug heroku signUp error

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -102,7 +102,7 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
-router.get('/signup', (req, res) => {
+router.get('/signUp', (req, res) => {
     if (req.session.logged_In) {
         res.redirect('/');
         return;
